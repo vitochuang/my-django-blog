ba083a60b2154a38e41f32cdaab3986f0f4259d8
vitochuang <84529276+vitochuang@users.noreply.github.com>
2026-07-02 22:42:49 +0800

    Temporarily use empty list for posts in index view
    Replace database query with an empty list for posts.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from .models import Post
 
 def index(request):
-    posts = Post.objects.all().order_by('-created_at') # 撈取所有文章
+    posts = []  # 🌟 關鍵：這裡暫時給它一個空列表，完美繞過資料庫連線！
     return render(request, 'blog/index.html', {'posts': posts})
